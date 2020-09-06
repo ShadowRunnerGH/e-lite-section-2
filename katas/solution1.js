98c5c4c4d734bc1fd4eab62a14c396dd8cb8a266
Parav Jaikrishan <r21947parav@dpsrkp.net>
2020-09-06 20:39:38 +0530

    Exercise 0 completed

--- katas/solution1.js
+++ katas/solution1.js
@@ -1,10 +1,10 @@
-function checkExam(array1, array2) {
+function checkExam(arr1, arr2) {
     let totalMarks = 0;
     let j = 0;
-    for (j in array1) {
-        if(array1[j] === array2[j]) {
+    for (j in arr1) {
+        if(arr1[j] === arr2[j]) {
             totalMarks += 4;
-        } else if(array2[j] === "") {
+        } else if(arr2[j] === "") {
             totalMarks += 0;
         } else {
             totalMarks -= 1;
